4c2c829d911fe0240914e6034bf30e8a76652b31
Drew Perry <drew@event.engineering>
2020-04-09 22:02:41 +0100

    Improved element-ui import

--- src/renderer/components/TestCard.vue
+++ src/renderer/components/TestCard.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="wrapper">
     <h1>Test Card. Yup.</h1>
-    
+    <el-button v-on:click="command('closeTestCard')" type="danger" icon="el-icon-close" circle></el-button>
   </div>
 </template>
 
 <script>
-  // import SystemInformation from './LandingPage/SystemInformation'
+const { ipcRenderer } = require('electron')
 
   export default {
     name: 'testcard',
     // components: { SystemInformation },
     methods: {
+      command: function (cmd) {
+        ipcRenderer.send('command', cmd)
+      }
     }
   }
 </script>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,7 +4,7 @@ import App from './App'
 import router from './router'
 
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/reset.css'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
