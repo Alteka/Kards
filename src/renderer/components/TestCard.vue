28876a46baf5a5fdd1ff8f0a52c284d1f3312faf
Drew Perry <drew@event.engineering>
2020-04-13 00:14:12 +0100

    Tidying up

--- src/renderer/components/TestCard.vue
+++ src/renderer/components/TestCard.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="wrapper">
-    <el-button v-on:click="closeTestCard()" type="danger" icon="el-icon-close" circle></el-button>
-    
     <Grid v-if="config.cardType == 'grid'" :config="config"></Grid>
     <Alteka v-if="config.cardType == 'alteka'" :config="config"></Alteka>
     <SMPTE v-if="config.cardType == 'smpte'" :config="config"></SMPTE>
     <ARIB v-if="config.cardType == 'arib'" :config="config"></ARIB>
     <Bars v-if="config.cardType == 'bars'" :config="config"></Bars>
     <Placeholder v-if="config.cardType == 'placeholder'" :config="config"></Placeholder>
-
   </div>
 </template>
 
@@ -20,11 +17,8 @@ import SMPTE from './TestCard/SMPTE'
 import ARIB from './TestCard/ARIB'
 import Bars from './TestCard/Bars'
 import Placeholder from './TestCard/Placeholder'
-var Mousetrap = require('mousetrap');
 
-Mousetrap.bind('up up down down left right left right b a enter', () => {
-  alert('Well done for trying the Konami code. We like you. Like... a lot.')
-})
+var Mousetrap = require('mousetrap');
 Mousetrap.bind('esc', function() { ipcRenderer.send('closeTestCard') }, 'keyup');
 
   export default {
--- src/renderer/components/TestCard/ARIB.vue
+++ src/renderer/components/TestCard/ARIB.vue
@@ -3,49 +3,55 @@
   
     <div class="row">
       <div class="grey40" style="width: 12.5%;"></div>
-      <div class="white"></div>
-      <div class="yellow100"></div>
-      <div class="cyan100"></div>
-      <div class="green100"></div>
-      <div class="magenta100"></div>
-      <div class="red100"></div>
-      <div class="blue100"></div>
+      <div style="width: 75%">
+          <div class="white"></div>
+          <div class="yellow100"></div>
+          <div class="cyan100"></div>
+          <div class="green100"></div>
+          <div class="magenta100"></div>
+          <div class="red100"></div>
+          <div class="blue100"></div>
+      </div>
       <div class="grey40" style="width: 12.5%;"></div>
     </div>
 
     <div class="row" style="height: 50%">
       <div class="grey40" style="width: 12.5%;"></div>
-      <div class="white75"></div>
-      <div class="yellow75"></div>
-      <div class="cyan75"></div>
-      <div class="green75"></div>
-      <div class="magenta75"></div>
-      <div class="red75"></div>
-      <div class="blue75"></div>
+      <div style="width: 75%">
+        <div class="white75"></div>
+        <div class="yellow75"></div>
+        <div class="cyan75"></div>
+        <div class="green75"></div>
+        <div class="magenta75"></div>
+        <div class="red75"></div>
+        <div class="blue75"></div>
+      </div>
       <div class="grey40" style="width: 12.5%;"></div>
     </div>
 
     <div class="row">
       <div class="white75" style="width: 12.5%"></div>
-      <div class="superblack"></div> 
-      <div style="width: 5.35%; background-color:rgba(16,16,16,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(38,38,38,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(60,60,60,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(82,82,82,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(104,104,104,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(125,125,125,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(148,148,148,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(169,169,169,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(191,191,191,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(213,213,213,1)"></div>
-      <div style="width: 5.35%; background-color:rgba(235,235,235,1)"></div>
-      <div style="width: 5.35%;" class="superwhite"></div>
+      <div style="width: 75%">
+          <div class="superblack"></div> 
+          <div style="width: 7.14285%; background-color:rgba(16,16,16,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(38,38,38,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(60,60,60,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(82,82,82,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(104,104,104,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(125,125,125,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(148,148,148,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(169,169,169,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(191,191,191,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(213,213,213,1)"></div>
+          <div style="width: 7.14285%; background-color:rgba(235,235,235,1)"></div>
+          <div style="width: 7.14285%;" class="superwhite"></div>
+      </div>
       <div style="width: 12.5%" class="white75"></div>
     </div>
 
     <div class="row">
       <div style="width: 12.5%;" class="black"></div>
-      <div style="width: 87.5%" class="ramp">RAMP</div>
+      <div style="width: 87.5%" class="ramp"></div>
     </div>
 
     <div class="row" style="height: 25%">
@@ -54,17 +60,18 @@
         <div style="width: 33%; background-color: rgb(135,177,180);"></div>
         <div style="width: 33%; background-color: rgb(128,177,74);"></div>
       </div>
-      <div class="black" style="width: 7.083333333%;"></div>
-      <div style="width: 3.64583%; background-color: rgb(12,12,12);"></div>
-      <div style="width: 3.541666%;" class="black"></div>
-      <div style="width: 3.64583%; background-color: rgb(20,20,20);"></div>
-      <div style="width: 3.541666%;" class="black"></div>
-      <div style="width: 3.64583%; background-color: rgb(25,25,25);"></div>
+      <div style="width: 75%">
+        <div class="black" style="width: 7.083333333%;"></div>
+        <div style="width: 3.64583%; background-color: rgb(12,12,12);"></div>
+        <div style="width: 3.541666%;" class="black"></div>
+        <div style="width: 3.64583%; background-color: rgb(20,20,20);"></div>
+        <div style="width: 3.541666%;" class="black"></div>
+        <div style="width: 3.64583%; background-color: rgb(25,25,25);"></div>
 
-      <div style="width: 12.395833%;" class="black"></div>
-      <div style="width: 22.8125%;" class="white75"></div>
-      <div style="width: 14.6875%;" class="black"></div>
-      
+        <div style="width: 12.395833%;" class="black"></div>
+        <div style="width: 22.8125%;" class="white75"></div>
+        <div style="width: 14.6875%;" class="black"></div>
+      </div>
       <div style="width: 12.5%">
         <div style="width: 33%; background-color: rgb(163,72,176);"></div>
         <div style="width: 33%; background-color: rgb(160,67,41);"></div>
@@ -77,12 +84,6 @@
 
 <script>
   export default {
-    data () {
-      return {
-        electron: process.versions.electron,
-        name: this.$route.name,
-      }
-    },
     props: {
       config: Object
     }
@@ -103,7 +104,7 @@
   height: 8.33333%;
 }
   .row div {
-    width: 10.71%;
+    width: 14.285%;
     height: 100%;
     float: left;
   }
