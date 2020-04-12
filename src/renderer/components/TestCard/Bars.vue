be0e58cef5f863b2d9b512315fe88942496fe3b1
Drew Perry <drew@event.engineering>
2020-04-12 20:36:01 +0100

    A crude 75% bars

--- src/renderer/components/TestCard/Bars.vue
+++ src/renderer/components/TestCard/Bars.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-   <h1>Colour Bars</h1>
+  <div id="bars">
+    <div id="bar1"></div>
+    <div id="bar2"></div>
+    <div id="bar3"></div>
+    <div id="bar4"></div>
+    <div id="bar5"></div>
+    <div id="bar6"></div>
+    <div id="bar7"></div>
+    <div id="bar8"></div>
   </div>
 </template>
 
@@ -19,5 +26,42 @@
 </script>
 
 <style scoped>
-  
+  #bars {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 100%;
+    width: 100%;
+    z-index: -100;
+    background: black;
+  }
+  #bars div {
+    float:left;
+    width: 12.5%;
+    height: 100%;
+  }
+  #bar1 {
+    background-color: rgba(255, 255, 255, 0.75)
+  }
+  #bar2 {
+    background-color: rgba(255, 255, 0, 0.75)
+  }
+  #bar3 {
+    background-color: rgba(0, 255, 255, 0.75)
+  }
+  #bar4 {
+    background-color: rgba(0, 255, 0, 0.75)
+  }
+  #bar5 {
+    background-color: rgba(255, 0, 255, 0.75)
+  }
+  #bar6 {
+    background-color: rgba(255, 0, 0, 0.75)
+  }
+  #bar7 {
+    background-color: rgba(0, 0, 255, 0.75)
+  }
+  #bar8 {
+    background-color: rgba(0, 0, 0, 1)
+  }
 </style>
