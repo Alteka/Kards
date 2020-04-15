64e89f377ba6c68626f7ee7b898cc4e9d4331120
Drew Perry <drew@event.engineering>
2020-04-15 16:31:36 +0100

    vfor bug fix

--- src/renderer/components/Control.vue
+++ src/renderer/components/Control.vue
@@ -18,7 +18,7 @@
 
 <el-row style="text-align: center">
     <el-radio-group fill="#7BB144" v-model="config.screen" :disabled="config.visible">
-      <el-radio-button v-for="scr in screens" :label="scr.id">
+      <el-radio-button v-for="scr in screens" :key="scr.id" :label="scr.id">
         <i v-if="scr.id != primaryScreen" class="el-icon-monitor"></i>
         <i v-if="scr.id == primaryScreen" class="el-icon-star-on"></i>
          {{ scr.size.width }} x {{ scr.size.height }}</el-radio-button>
