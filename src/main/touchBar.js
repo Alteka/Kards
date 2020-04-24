c809db3158ba110539bfc7a5351161180968c9f7
Drew Perry <drew@event.engineering>
2020-04-24 23:08:59 +0100

    Improved touchbar

--- src/main/touchBar.js
+++ src/main/touchBar.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow, TouchBar, nativeImage } = require('electron')
-const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer, TouchBarPopover } = TouchBar
 const path = require('path')
 
 let config = {}
@@ -29,18 +29,85 @@ const enableButton = new TouchBarButton({
     }
   })
 
+  const showInfoButton = new TouchBarButton({
+    label: 'Show Info',
+    icon: disabled,
+    iconPosition: 'left',
+    click: () => {
+        config.showInfo = !config.showInfo
+        window.webContents.send('config', config)
+    }
+  })
+
+  const cardPopover = new TouchBarPopover({
+      label: 'Card Type',
+      items: [
+        new TouchBarButton({
+            label: 'Alteka',
+            click: () => {
+                config.cardType = 'alteka'
+                window.webContents.send('config', config)
+            }
+          }),
+          new TouchBarButton({
+            label: 'SMPTE',
+            click: () => {
+                config.cardType = 'smpte'
+                window.webContents.send('config', config)
+            }
+          }),
+          new TouchBarButton({
+            label: 'ARIB',
+            click: () => {
+                config.cardType = 'arib'
+                window.webContents.send('config', config)
+            }
+          }),
+          new TouchBarButton({
+            label: 'Bars',
+            click: () => {
+                config.cardType = 'bars'
+                window.webContents.send('config', config)
+            }
+          }),
+          new TouchBarButton({
+            label: 'Grid',
+            click: () => {
+                config.cardType = 'grid'
+                window.webContents.send('config', config)
+            }
+          }),
+          new TouchBarButton({
+            label: 'Ramp',
+            click: () => {
+                config.cardType = 'ramp'
+                window.webContents.send('config', config)
+            }
+          }),
+          new TouchBarButton({
+            label: 'Placeholder',
+            click: () => {
+                config.cardType = 'placeholder'
+                window.webContents.send('config', config)
+            }
+          })
+      ]
+  })
+
   
   const touchBar = new TouchBar({
     items: [
-      enableButton, motionButton
+      enableButton, motionButton, showInfoButton, cardPopover
     ]
   })
 
   function setConfig(c) {  
     config = c
     
-enableButton.icon = config.visible ? enabled : disabled
-motionButton.icon = config.animated? enabled : disabled
+    enableButton.icon = config.visible ? enabled : disabled
+    motionButton.icon = config.animated ? enabled : disabled
+    showInfoButton.icon = config.showInfo ? enabled : disabled
+
 
   }
 
