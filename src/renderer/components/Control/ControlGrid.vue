277119f86e4d514209dcfbf21a4a173b9eab2197
Drew Perry <drew@event.engineering>
2020-04-29 20:46:21 +0100

    Grid control improvements

--- src/renderer/components/Control/ControlGrid.vue
+++ src/renderer/components/Control/ControlGrid.vue
@@ -6,18 +6,18 @@
             <el-color-picker v-model="grid.bg"></el-color-picker>
           </el-form-item>
         </el-col>
-        <el-col :span="16">
+        <el-col :span="8">
           <el-form-item label="Crosshair">
             <el-color-picker v-model="grid.crosshair"></el-color-picker>
           </el-form-item>
         </el-col>
-      </el-row>
-      <el-row>
         <el-col :span="8">
           <el-form-item label="Lines">
             <el-color-picker v-model="grid.lines"></el-color-picker>
           </el-form-item>
         </el-col>
+      </el-row>
+      <el-row>
         <el-col :span="16">
           <el-form-item label="Size (pixels)">
             <el-input-number v-model="grid.size" :step="5" size="mini"></el-input-number>
