6fe27945d0b75250470ab9eb1149e87b0e66c957
Drew Perry <me@drewperry.co.uk>
2020-06-17 22:49:43 +0100

    Remove unreliable touch bar bits. Simplified.

--- src/main/touchBar.js
+++ src/main/touchBar.js
@@ -43,65 +43,10 @@ const enableButton = new TouchBarButton({
     }
   })
 
-  const cardPopover = new TouchBarPopover({
-      label: 'Card Type',
-      items: [
-        new TouchBarButton({
-            label: 'Alteka',
-            click: () => {
-                config.cardType = 'alteka'
-                window.webContents.send('config', config)
-            }
-          }),
-          new TouchBarButton({
-            label: 'SMPTE',
-            click: () => {
-                config.cardType = 'smpte'
-                window.webContents.send('config', config)
-            }
-          }),
-          new TouchBarButton({
-            label: 'ARIB',
-            click: () => {
-                config.cardType = 'arib'
-                window.webContents.send('config', config)
-            }
-          }),
-          new TouchBarButton({
-            label: 'Bars',
-            click: () => {
-                config.cardType = 'bars'
-                window.webContents.send('config', config)
-            }
-          }),
-          new TouchBarButton({
-            label: 'Grid',
-            click: () => {
-                config.cardType = 'grid'
-                window.webContents.send('config', config)
-            }
-          }),
-          new TouchBarButton({
-            label: 'Ramp',
-            click: () => {
-                config.cardType = 'ramp'
-                window.webContents.send('config', config)
-            }
-          }),
-          new TouchBarButton({
-            label: 'Placeholder',
-            click: () => {
-                config.cardType = 'placeholder'
-                window.webContents.send('config', config)
-            }
-          })
-      ]
-  })
-
   
   const touchBar = new TouchBar({
     items: [
-      enableButton, motionButton, showInfoButton, cardPopover
+      enableButton, showInfoButton, motionButton
     ]
   })
 
