e2a70dab1d53eacd9fd8eb668a9209bc230f0672
Drew Perry <me@drewperry.co.uk>
2020-07-28 18:05:50 +0100

    Better sizing and styling of info circle

--- src/renderer/components/TestCard/InfoCircle.vue
+++ src/renderer/components/TestCard/InfoCircle.vue
@@ -2,7 +2,7 @@
   <transition name="fade">
     <div v-if="config.showInfo" class="info">
       <div class="infoText">
-        <strong>{{ config.name}}</strong><br />
+        <strong>{{ config.name}}</strong><br /><br v-if="spacer" />
         {{ cardSize }}
       </div>
     </div>
@@ -18,6 +18,11 @@ import ResizeText from 'vue-resize-text'
     props: {
       config: Object,
       cardSize: String
+    },
+    computed: {
+      spacer: function() {
+        return this.config.name.length < 30 ? true : false
+      }
     }
   }
 </script>
@@ -25,12 +30,12 @@ import ResizeText from 'vue-resize-text'
 <style scoped>
   .info {
     position: absolute;
-    font-size: 24px;
-    width: 200px;
-    height: 200px;
+    font-size: 56px;
+    width: 500px;
+    height: 500px;
     margin: auto;
-    left: calc(50% - 100px);
-    top: calc(50% - 100px);
+    left: calc(50% - 250px);
+    top: calc(50% - 250px);
     text-align: center;
     text-shadow: 0px 0px 5px rgba(0, 10, 0, 1);
     background: rgba(0,0,0,0.2);
@@ -51,5 +56,52 @@ import ResizeText from 'vue-resize-text'
 }
 .fade-enter, .fade-leave-to {
   opacity: 0;
+}
+
+
+@media screen and (max-height: 1600px), (max-width: 1600px) {
+  .info {
+    font-size: 48px;
+    width: 400px;
+    height: 400px;
+    left: calc(50% - 200px);
+    top: calc(50% - 200px);
+  }
+}
+@media screen and (max-height: 1300px), (max-width: 1300px) {
+  .info {
+    font-size: 36px;
+    width: 300px;
+    height: 300px;
+    left: calc(50% - 150px);
+    top: calc(50% - 150px);
+  }
+}
+@media screen and (max-height: 900px), (max-width: 900px) {
+  .info {
+    font-size: 24px;
+    width: 200px;
+    height: 200px;
+    left: calc(50% - 100px);
+    top: calc(50% - 100px);
+  }
+}
+@media screen and (max-height: 500px), (max-width: 500px) {
+  .info {
+    font-size: 18px;
+    width: 150px;
+    height: 150px;
+    left: calc(50% - 75px);
+    top: calc(50% - 75px);
+  }
+}
+@media screen and (max-height: 300px), (max-width: 300px) {
+  .info {
+    font-size: 12px;
+    width: 100px;
+    height: 100px;
+    left: calc(50% - 50px);
+    top: calc(50% - 50px);
+  }
 }
 </style>
