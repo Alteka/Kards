4b7f4136142fa360272357e12cd18c8abc79d6ff
Drew Perry <me@drewperry.co.uk>
2020-08-07 22:40:27 +0100

    Misc 1.0 tweaks

--- src/renderer/components/Control/ControlScreen.vue
+++ src/renderer/components/Control/ControlScreen.vue
@@ -1,21 +1,12 @@
 <template>
   <div> 
     <svg :view-box.camel="viewBox" style="width: 90%; margin-left: 5%; margin-top: 10px; height: 125px;">
-      <defs>
-        <radialGradient id="selected">
-          <stop offset="0%" stop-color="#6ab42f"/>
-          <stop offset="100%" stop-color="#588033"/>
-        </radialGradient>
-        <radialGradient id="gradient">
-          <stop offset="0%" stop-color="#666"/>
-          <stop offset="100%" stop-color="#444"/>
-        </radialGradient>
-      </defs>
       <g v-for="scr in screens" :key="scr.id" v-on:click="config.screen = scr.id">
-        <rect :x="scr.bounds.x" :y="scr.bounds.y" :width="scr.bounds.width" :height="scr.bounds.height" fill="url(#gradient)" style="stroke-width:25;stroke:#3d3d3d;" />
-        <rect :x="scr.bounds.x" :y="scr.bounds.y" :width="scr.bounds.width" :height="scr.bounds.height" fill="url(#selected)" style="stroke-width:25;stroke:#3d3d3d;" v-if="config.screen == scr.id" />
+        <rect :x="scr.bounds.x" :y="scr.bounds.y" :width="scr.bounds.width" :height="scr.bounds.height" fill="#555" style="stroke-width:25;stroke:#3d3d3d;" />
+        <rect :x="scr.bounds.x" :y="scr.bounds.y" :width="scr.bounds.width" :height="scr.bounds.height" fill="#6ab42f" style="stroke-width:25;stroke:#3d3d3d;" v-if="config.screen == scr.id" />
         <text :x="scr.bounds.x + scr.bounds.width/2" :y="scr.bounds.y + scr.bounds.height/1.25" :width="scr.bounds.width" :height="scr.bounds.height" font-family="Verdana" :font-size="scr.bounds.height/5" text-anchor="middle" fill="white">{{ scr.description }}</text>
         <text :x="scr.bounds.x + scr.bounds.width/2" :y="scr.bounds.y + scr.bounds.height*0.45" :width="scr.bounds.width" :height="scr.bounds.height" text-anchor="middle" fill="white" :font-size="scr.bounds.height/3" class="fa">{{ scr.icon }}</text>
+        <text :x="scr.bounds.x + scr.bounds.width/2" :y="scr.bounds.y + scr.bounds.height*0.36" :width="scr.bounds.width" :height="scr.bounds.height" text-anchor="middle" fill="white" :font-size="scr.bounds.height/6" class="fa" v-if="config.screen == scr.id && config.visible">{{ "\uf00c" }}</text>
       </g>
     </svg>
   </div>
@@ -31,6 +22,7 @@ const { screen } = require('electron').remote
       return {
         screens: screen.getAllDisplays(),
         primaryScreen: screen.getPrimaryDisplay().id,
+        check: "\uf00c",
         viewBox: '0 0 0 0'
       }
     },
