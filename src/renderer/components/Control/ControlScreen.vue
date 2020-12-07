768c67f80acda6de7a653c3896b4ebf238bed1a2
Drew Perry <me@drewperry.co.uk>
2020-12-07 21:11:52 +0000

    Removed unused variable in config

--- src/renderer/components/Control/ControlScreen.vue
+++ src/renderer/components/Control/ControlScreen.vue
@@ -82,8 +82,6 @@ const { ipcRenderer } = require('electron')
               this.config.left = 0
               this.config.width = scr.size.width
               this.config.height = scr.size.height
-              this.config.maxWidth = scr.size.width
-              this.config.maxHeight = scr.size.height
             }
           }
         }
