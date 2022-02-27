a3ac1bb8b1024059d3a0b85063ba33fe70ada928
Drew Perry <me@drewperry.co.uk>
2022-02-27 20:55:44 +0000

    #83 Properly support deep merge in put api

--- src/rest.js
+++ src/rest.js
@@ -80,8 +80,13 @@ class restServer extends EventEmitter {
 
     _handlePut(req, res) {
         if (Object.keys(req.body).length > 0) {
-            const returnedTarget = Object.assign(this.config, req.body)
+            const returnedTarget = this.mergeDeep(this.config, req.body)
             this.emit('updateConfig', returnedTarget)
+
+            delete returnedTarget.alteka.logo
+            delete returnedTarget.audio.textData
+            delete returnedTarget.audio.fileData
+            delete returnedTarget.audio.voiceData
             res.send(returnedTarget)
         } else {
             res.send('Poorly formatted request')
@@ -90,6 +95,27 @@ class restServer extends EventEmitter {
     
     updateConfig(c) {
         this.config = c
+    }
+
+    isObject(item) {
+        return (item && typeof item === 'object' && !Array.isArray(item));
+    }
+  
+    mergeDeep(target, ...sources) {
+        if (!sources.length) return target;
+        const source = sources.shift();
+  
+        if (this.isObject(target) && this.isObject(source)) {
+        for (const key in source) {
+            if (this.isObject(source[key])) {
+                if (!target[key]) Object.assign(target, { [key]: {} });
+                    this.mergeDeep(target[key], source[key]);
+                } else {
+                    Object.assign(target, { [key]: source[key] });
+                }
+            }
+        }
+        return this.mergeDeep(target, ...sources);
     }
 }
 
