9bfbecaeff8418495903eee405f42b5c2d7ce860
Drew Perry <me@drewperry.co.uk>
2022-06-12 13:38:11 +0100

    Add promise for whenReady to fix rollbar 32

--- src/background.js
+++ src/background.js
@@ -80,7 +80,9 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  app.whenReady().then(() => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()  
+   })
 })
 
 app.on('ready', async () => {
